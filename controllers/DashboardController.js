3b4479752375d741c3f3cd9f6aaa3ac6875c6ad2
Dilshan-Dekumpitiya <dilshandekumpitiya@gmail.com>
2024-04-14 22:00:03 +0530

    convert to SPA

---
+++ controllers/DashboardController.js
@@ -0,0 +1,46 @@
+/** Load Page View Change and convert to SPA **/
+$(document).ready(function () {
+    const hide_all_contents = function () {
+        $('#HomeSection').css('display', 'none');
+        $('#CustomerSection').css('display', 'none');
+        $('#ItemSection').css('display', 'none');
+        $('#OrderSection').css('display', 'none');
+        $('#OrderDetails').css('display', 'none');
+    };
+
+    hide_all_contents();
+    
+    $('#HomeSection').css('display', 'block');
+
+    $('#homeBtn').click(function () {
+        hide_all_contents();
+        $('#HomeSection').css('display', 'block');
+    });
+
+    $('#customerBtn').click(function () {
+        hide_all_contents();
+        $('#CustomerSection').css('display', 'block');
+        $("#txtCustomerId").val(generateCustomerID());
+    });
+
+    $('#itemBtn').click(function () {
+        hide_all_contents();
+        $('#ItemSection').css('display', 'block');
+        $("#txtItemsId").val(generateItemID());
+    });
+
+    $('#orderBtn').click(function () {
+        hide_all_contents();
+        $('#OrderSection').css('display', 'block');
+        $("#orderId").val(generateOrderID());
+        setCurrentDate();
+    });
+
+    $('#orderDetailsBtn').click(function () {
+        hide_all_contents();
+        $('#OrderDetails').css('display', 'block');
+        loadAllOrders();
+        loadAllOrderDetails();
+    });
+});
+
